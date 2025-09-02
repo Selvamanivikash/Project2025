b261fef7c7f52d4ffeb2af39f6a7f2e9e7e42327
Selvamanivikash <selvamanivikash424@gmail.com>
2025-09-02 19:53:43 +0530

    message

---
+++ src/Sample.java
@@ -0,0 +1,15 @@
+
+public class Sample {
+	private void kerala() {
+		System.out.println("Kerala");
+	}
+	private void tamilNadu() {
+		System.out.println("TamilNadu");
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.kerala();
+		s.tamilNadu();
+	}
+
+}
