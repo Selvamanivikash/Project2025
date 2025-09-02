231b2bbddb6d168ea536ac343f4b482746f1d438
Selvamanivikash <selvamanivikash424@gmail.com>
2025-09-02 20:28:59 +0530

    Member1 has completed scripting

--- src/Sample.java
+++ src/Sample.java
@@ -6,6 +6,12 @@ public class Sample {
 	private void tamilNadu() {
 		System.out.println("TamilNadu");
 	}
+	private void m1() {
+		System.out.println("m1");
+	}
+	private void m2() {
+		System.out.println("m2");
+	}
 	public static void main(String[] args) {
 		Sample s = new Sample();
 		s.kerala();
